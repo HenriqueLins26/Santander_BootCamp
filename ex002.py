7b2783eef8e8d4107a965a54de9a9f4d10e481ef
Henrique Lins <113059590+HenriqueLins26@users.noreply.github.com>
2025-06-15 16:45:01 -0300

    exercícios

---
+++ ex002.py
@@ -0,0 +1,7 @@
+nome = str(input("Informe o seu nome: "))
+idade = int(input('Informe a sua idade: '))
+
+print(nome, idade)
+print(nome, idade, end="...\n")
+print(nome, idade, sep="#", end="...\n")
+print(nome, idade, sep="#")
